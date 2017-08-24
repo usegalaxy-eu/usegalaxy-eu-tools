025ef9fb665a862992eb4243fc19b2d3c89c8987
E Rasche <hxr@hx42.org>
2017-08-24 14:09:47 +0200

    cleanup

--- scripts/identify-unpinned.py
+++ scripts/identify-unpinned.py
@@ -6,16 +6,5 @@ for file in glob.glob("*.yaml"):
         w = yaml.load(handle)
 
     for tool in w['tools']:
-        print(tool)
-        if 'changeset_revision' not in tool:
-            print(tool)
-
-for file in glob.glob("*.yml"):
-    print("Processing %s" % file)
-    with open(file, 'r') as handle:
-        w = yaml.load(handle)
-
-    for tool in w['tools']:
-        print(tool)
         if 'changeset_revision' not in tool:
             print(tool)
