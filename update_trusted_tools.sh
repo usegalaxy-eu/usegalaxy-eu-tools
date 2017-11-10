e1e888fed7b3d3086a656debee7bb520777e8270
Bjoern Gruening <bjoern.gruening@gmail.com>
2017-11-10 22:05:07 +0100

    add trusted repo shell script

---
+++ update_trusted_tools.sh
@@ -0,0 +1,6 @@
+export PATH=/usr/local/tools/_conda/bin/:$PATH
+source activate ephemeris
+
+shed-install -t tools_iuc.yaml -a $GALAXY_API_KEY --galaxy https://galaxy.uni-freiburg.de
+
+/usr/local/galaxy/galaxy-fr-tools/fix_conda_env.sh
