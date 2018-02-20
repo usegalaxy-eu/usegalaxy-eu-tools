0e0319e52adcd6e4c91be02d0ab15eca6ba95ed3
E Rasche <hxr@hx42.org>
2018-02-20 15:53:28 +0100

    Rewrote update-trusted as update-tool with better features

--- scripts/identify-unpinned.py
+++ scripts/identify-unpinned.py
@@ -1,10 +1,32 @@
 import yaml
-import glob
-for file in glob.glob("*.yaml.lock"):
-    print("Processing %s" % file)
-    with open(file, 'r') as handle:
-        w = yaml.load(handle)
+import os
+import sys
+import argparse
 
-    for tool in w['tools']:
+
+def update_file(fn):
+    # If a lock file exists, load it from that file
+    with open(fn + '.lock', 'r') as handle:
+        locked = yaml.load(handle)
+
+    exit_code = 0
+    # As here we add any new tools in.
+    unpinned = []
+    for tool in locked['tools']:
         if 'changeset_revision' not in tool:
-            print(tool)
+            exit_code = 1
+            unpinned.append(tool)
+
+    if len(unpinned) > 0:
+        print("Unpinned tools in %s:" % fn)
+        for tool in unpinned:
+            print("  %s/%s" % (tool['owner'], tool['name']))
+
+    sys.exit(exit_code)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('fn', type=argparse.FileType('r'), help="Tool.yaml file")
+    args = parser.parse_args()
+    update_file(args.fn.name)
--- scripts/update-tool.py
+++ scripts/update-tool.py
@@ -10,31 +10,23 @@ from bioblend import toolshed
 ts = toolshed.ToolShedInstance(url='https://toolshed.g2.bx.psu.edu')
 
 
-def update_file(fn, dry):
-    with open(fn, 'r') as handle:
-        unlocked = yaml.load(handle)
-    # If a lock file exists, load it from that file
-    if os.path.exists(fn + '.lock'):
-        with open(fn + '.lock', 'r') as handle:
-            locked = yaml.load(handle)
-    else:
-        # Otherwise just clone the "unlocked" list.
-        locked = copy.deepcopy(unlocked)
-
-    # Extract the name of every tool. This will potentially be outdated if
-    # someone has added something to the main file. this is intentional.
-    locked_tools = [x['name'] for x in locked['tools']]
-
-    # As here we add any new tools in.
-    for tool in unlocked['tools']:
-        if tool['name'] not in locked_tools:
-            # Add it to the set of locked tools.
-            locked['tools'].append(tool)
+def update_file(fn, owner=None, name=None, without=False):
+    with open(fn + '.lock', 'r') as handle:
+        locked = yaml.load(handle)
 
     # Update any locked tools.
     for tool in locked['tools']:
-        # Should we trust this?
-        # get CR
+        # If without, then if it is lacking, we should exec.
+        if without:
+            if 'changeset_revision' in tool:
+                continue
+
+        if not without and owner and tool['owner'] != owner:
+            continue
+
+        if not without and name and tool['name'] != name:
+            continue
+
         try:
             revs = ts.repositories.get_ordered_installable_revisions(tool['name'], tool['owner'])
         except Exception as e:
@@ -42,8 +34,6 @@ def update_file(fn, dry):
             continue
 
         print("Found newer revision of %s/%s (%s)" % (tool['owner'], tool['name'], revs[0]))
-        if dry:
-            continue
 
         # Get latest rev, if not already added, add it.
         if 'changeset_revision' not in tool:
@@ -61,6 +51,8 @@ def update_file(fn, dry):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('fn', type=argparse.FileType('r'), help="Tool.yaml file")
-    parser.add_argument('--dry-run', action='store_true', help="Trust all listed tools in the file, i.e. add the latest changest for them.")
+    parser.add_argument('--owner', help="Repository owner to filter on, anything matching this will be updated")
+    parser.add_argument('--name', help="Repository name to filter on, anything matching this will be updated")
+    parser.add_argument('--without', action='store_true', help="If supplied will ignore any owner/name and just automatically add the latest hash for anything lacking one.")
     args = parser.parse_args()
-    update_file(args.fn.name, dry=args.dry_run)
+    update_file(args.fn.name, owner=args.owner, name=args.name, without=args.without)
