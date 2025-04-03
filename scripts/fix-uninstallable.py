1ad734821ae21593067b783ed65b22a0f4a4fa9a
Matthias Bernt <m.bernt@ufz.de>
2025-04-03 14:33:37 +0200

    also add next installable versions of not yet in

--- scripts/fix-uninstallable.py
+++ scripts/fix-uninstallable.py
@@ -81,13 +81,14 @@ def fix_uninstallable(lockfile_name, toolshed_url):
             all_revisions = get_all_revisions(toolshed_url, name, owner)
             # all_versions = get_all_versions(toolshed_url, name, owner, all_revisions)
 
-        remove = []
+        to_remove = []
+        to_append = []
         for cur in tool["revisions"]:
             if cur in ordered_installable_revisions:
                 continue
             if cur not in all_revisions:
                 print(f"{cur} is not a valid revision of {name} {owner}")
-                remove.append(cur)
+                to_remove.append(cur)
                 continue
             start = all_revisions.index(cur)
             nxt = None
@@ -95,19 +96,17 @@ def fix_uninstallable(lockfile_name, toolshed_url):
                 if all_revisions[i] in ordered_installable_revisions:
                     nxt = all_revisions[i]
             if nxt:
-                if nxt in tool["revisions"]:
-                    print(f"remove {cur} in favor of {nxt} {name} {owner}")
-                    remove.append(cur)
-                else:
-                    # not adding, will be done in next update anyway
-                    print(
-                        f"NOT removing {cur} since {nxt} not installed {name} {owner}"
-                    )
+                print(f"remove {cur} in favor of {nxt} {name} {owner}")
+                to_remove.append(cur)
+                if nxt not in tool["revisions"]:
+                    print(f"adding {nxt} which was absent so far {name} {owner}")
+                    to_append(nxt)
             else:
                 print(f"Could not determine next revision for {cur} {name} {owner}")
 
-        for r in remove:
+        for r in to_remove:
             tool["revisions"].remove(r)
+        tool["revisions"].extend(to_append)
 
     with open(lockfile_name, "w") as handle:
         yaml.dump(lockfile, handle, default_flow_style=False)
