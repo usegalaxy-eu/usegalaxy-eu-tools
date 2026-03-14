b953a61aa3aec605d1d5ec7c02750acb4181bda8
Bjoern Gruening <bjoern.gruening@gmail.com>
2026-03-14 18:11:41 +0100

    use direct TS lookup and not bioblend search

--- scripts/pr-check.py
+++ scripts/pr-check.py
@@ -1,22 +1,43 @@
 import sys
+import requests
 import yaml
 
-from bioblend import toolshed 
-
-ts = toolshed.ToolShedInstance(url='https://toolshed.g2.bx.psu.edu')
+TOOLSHED_API = "https://toolshed.g2.bx.psu.edu/api/repositories"
 fn = sys.argv[1]
+
+
+def load_tools(path):
+    with open(path) as handle:
+        data = yaml.safe_load(handle) or {}
+
+    return {
+        (tool["name"], tool["owner"])
+        for tool in data.get("tools", [])
+    }
+
+
+def tool_exists(name, owner):
+    response = requests.get(
+        TOOLSHED_API,
+        params={"name": name, "owner": owner, "page_size": 1},
+        timeout=30,
+    )
+    response.raise_for_status()
+
+    return any(
+        repo["name"] == name and repo.get("owner") == owner
+        for repo in response.json()
+    )
 
 # never mind about fancy yaml linting, let's just make sure the files are openable
 sys.stdout.write('Checking modified yaml file {}...\n'.format(fn))
-with open(fn) as f: 
-    yml = [n['name'] for n in yaml.safe_load(f)['tools']]
+yml = load_tools(fn)
+yml_lock = load_tools('{}.lock'.format(fn))
 
-with open('{}.lock'.format(fn)) as f:
-    yml_lock = [n['name'] for n in yaml.safe_load(f)['tools']]
+new_tools = sorted(yml - yml_lock)
 
-new_tools = [n for n in yml if n not in yml_lock]
-
-for tool in new_tools:  # check all new tools are in the tool shed
-    sys.stdout.write('Checking new tool {} is in the toolshed...\n'.format(tool))
-    search_hits = [hit['repository']['name'] for hit in ts.repositories.search_repositories(tool,page_size=600)['hits']]
-    assert tool in search_hits, '{} not in toolshed.'.format(tool)
+for (name, owner) in new_tools:  # check all new tools are in the tool shed
+    sys.stdout.write(
+        'Checking new tool {}/{} is in the toolshed...\n'.format(owner, name)
+    )
+    assert tool_exists(name, owner), '{}/{} not in toolshed.'.format(owner, name)
